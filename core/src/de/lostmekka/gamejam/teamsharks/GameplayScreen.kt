0ef060e9c95f8c6ce29f6041afa885ec86891e38
3m5.Stefan Beyer <Stefan.Beyer@3m5.de>
2021-10-30 10:05:51 +0200

    added libktx

---
+++ core/src/de/lostmekka/gamejam/teamsharks/GameplayScreen.kt
@@ -0,0 +1,25 @@
+package de.lostmekka.gamejam.teamsharks
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxScreen
+import ktx.graphics.use
+
+class GameplayScreen : KtxScreen {
+    val font = BitmapFont()
+    val batch = SpriteBatch().apply {
+        color = Color.WHITE
+    }
+
+    override fun render(delta: Float) {
+        batch.use {
+            font.draw(it, "Hello Kotlin!", 100f, 100f)
+        }
+    }
+
+    override fun dispose() {
+        font.dispose()
+        batch.dispose()
+    }
+}
