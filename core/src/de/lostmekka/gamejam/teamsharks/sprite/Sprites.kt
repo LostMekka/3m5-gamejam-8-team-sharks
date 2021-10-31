ede0f322d18aa8496232cc7ca5e536d39f8db867
3m5.Stefan Beyer <Stefan.Beyer@3m5.de>
2021-10-31 10:10:32 +0100

    load ore png

--- core/src/de/lostmekka/gamejam/teamsharks/sprite/Sprites.kt
+++ core/src/de/lostmekka/gamejam/teamsharks/sprite/Sprites.kt
@@ -1,6 +1,7 @@
 package de.lostmekka.gamejam.teamsharks.sprite
 
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import de.lostmekka.gamejam.teamsharks.data.ResourceType
 import ktx.assets.disposeSafely
 
@@ -10,6 +11,14 @@ class Sprites {
     val resourceIcons = ResourceType.values().associateWith { loadTexture("sprites/resources/$it.png") }
     val backgroundEarth = loadTexture("sprites/background/BgEarth.png")
     val backgroundFactory = loadTexture("sprites/background/Factory.png")
+    val resourceDeposits = loadTexture("sprites/resources/Ores.png")
+        .split(96, 96, 5, 1)
+        .let {
+            mapOf(
+                ResourceType.IronOre to it[3],
+                ResourceType.CopperOre to it[1],
+            )
+        }
 
     fun disposeSafely() {
         allSprites.disposeSafely()
@@ -20,4 +29,19 @@ class Sprites {
             it.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
             allSprites += it
         }
+}
+
+private fun Texture.split(
+    spriteWidth: Int,
+    spriteHeight: Int,
+    atlasWidth: Int,
+    atlasHeight: Int,
+): MutableList<TextureRegion> {
+    val sprites = mutableListOf<TextureRegion>()
+    for (y in 0 until atlasHeight) {
+        for (x in 0 until atlasWidth) {
+            sprites += TextureRegion(this, x * spriteWidth, y * spriteHeight, spriteWidth, spriteHeight)
+        }
+    }
+    return sprites
 }
